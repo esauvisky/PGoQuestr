f6f16e7f534e1d2e813599fe2c00d65e9bd840d2
Emiliano Sauvisky <esauvisky@gmail.com>
2019-03-30 06:32:54 -0300

    lots of stuff

--- questr.py
+++ questr.py
@@ -16,7 +16,7 @@ from COOLmeDOWN import splitCoords, calculate, calculateCD
 
 import logging
 from colorlog import ColoredFormatter
-logger = logging.getLogger('ivcheck')
+logger = logging.getLogger('questr')
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
@@ -72,18 +72,19 @@ class Main:
         if str(keycode).lower in self.config['waits']:
             await asyncio.sleep(self.config['waits'][str(keycode).lower])
 
-    async def deal_with_blocking_dialogs(self):
+    async def check_where_the_hell_are_we(self):
         screencap = await self.p.screencap()
-        text_oh = screencap.crop(self.config['locations']['oh_hatching_box'])
-        text_oh = self.tool.image_to_string(text_oh).replace("\n", " ")
+
         text_gps = screencap.crop(self.config['locations']['im_a_passenger_button_box'])
         text_gps = self.tool.image_to_string(text_gps).replace("\n", " ")
-
         if 'PASSENGER' in text_gps:
             logger.error("I'M NOT A PASSENGER, I'M A SPOOFER, WHEN ARE YOU GOING TO UNDERSTAND?!")
             await self.tap('im_a_passenger_button_box')
-            return True
-        elif 'Oh' in text_oh or '?' in text_oh:
+            return 'on_passenger'
+
+        text_oh = screencap.crop(self.config['locations']['oh_hatching_box'])
+        text_oh = self.tool.image_to_string(text_oh).replace("\n", " ")
+        if 'Oh' in text_oh or '?' in text_oh:
             logger.error('Oh, look at that, we just hatched an egg, lol.')
             # click anywhere, twice (we click on i'm a passenger button)
             await self.tap('im_a_passenger_button_box')
@@ -92,9 +93,16 @@ class Main:
             await asyncio.sleep(20)
             # close pokemon screen
             await self.tap('x_button')
-            return True
+            return 'on_egg'
 
-        return False
+        text_shop = screencap.crop(self.config['locations']['shop_button_text_box'])
+        text_shop = self.tool.image_to_string(text_shop).replace("\n", " ")
+        if 'SHOP' in text_shop:
+            logger.error('Looks like somehow we went onto the menu... lolz')
+            await self.tap('x_button')
+            return 'on_menu'
+
+        return 'on_world'
 
     async def cap_and_crop(self, location):
         screencap = await self.p.screencap()
@@ -102,6 +110,40 @@ class Main:
         text = self.tool.image_to_string(crop).replace("\n", " ")
         logger.info('[OCR] Found text: %s', text)
         return text
+
+    async def spin_pokestop(self):
+        logger.info('Clicking PokeStop')
+        await self.tap('pokestop')
+
+        logger.info('Spinning...')
+        await self.swipe('spin_swipe', 300)
+
+        await asyncio.sleep(0.3)
+        screencap = await self.p.screencap()
+
+        crop = screencap.crop(self.config['locations']['your_bag_is_full_text_box'])
+        crop.show()
+        text = self.tool.image_to_string(crop).replace("\n", " ")
+        if 'Bag' in text:
+            logger.error('Shit, we need to clear our bag and try again.')
+            await self.tap('x_button')
+            # self.clear_bag()
+            await self.spin_pokestop()
+            return
+
+        crop = screencap.crop(self.config['locations']['your_bag_is_full_text_box'])
+        text = self.tool.image_to_string(crop).replace("\n", " ")
+        if 'again' in text:
+            logger.error("Oh, we're being too precoce. Lets give it a few secs and try again...")
+            await asyncio.sleep(5)
+            await self.tap('x_button')
+            # self.clear_bag()
+            await self.spin_pokestop()
+            return
+
+        logger.info('Leaving PokeStop')
+        await self.tap('x_button')
+
 
     async def start(self):
         await self.p.set_device(self.args.device_id)
@@ -112,22 +154,16 @@ class Main:
         actions_so_far = 0
         for num, quest in enumerate(quest_list, start=1):
             quest_coords = splitCoords(quest)
-            time_start = time.time()
 
             logger.info('Teleporting to quest number %s, coords: %s', num, quest_coords)
             await self.p.run(["adb", "-s", await self.p.get_device(), "shell", 'am start-foreground-service -a theappninjas.gpsjoystick.TELEPORT --ef lat {} --ef lng {}'.format(*quest_coords)])
-            await asyncio.sleep(20)
+            await asyncio.sleep(10)
 
-            await self.deal_with_blocking_dialogs
+            while await self.check_where_the_hell_are_we() is not 'on_world':
+                logger.info("We still seem to be loading")
+                continue
 
-            await self.tap('pokestop')
-            logger.info('Clicking PokeStop')
-
-            logger.info('Spinning...')
-            await self.swipe('spin_swipe', 300)
-
-            logger.info('Leaving PokeStop')
-            await self.tap('x_button')
+            await self.spin_pokestop()
 
             # Validate/Check/DO action (for other actions rather than spinning)
             if args.action == 'spin':
@@ -135,25 +171,29 @@ class Main:
 
             # Finished, can claim quest
             if actions_so_far == args.num:
-                actions_so_far = 0
                 await self.tap('quest_button')
-                text = await self.cap_and_crop('claim_reward_box')
-                if 'CLAIM REWARD' in text:
-                    await self.tap('claim_reward_box')
-                else:
-                    logger.error("Seems we didn't complete any quest! Lets try again...")
-                    num = num - 1
+
+                while 'CLAIM REWARD' not in await self.cap_and_crop('claim_reward_box'):
+                    logger.error("Seems it didn't work. Lets try to spin again from other angle...")
+                    await self.swipe('spin_swipe', 800)
+                    await self.tap('x_button')
+                    await self.spin_pokestop()
+                    await self.tap('quest_button')
                     continue
+
+                logger.warning("Cool, it's ok now.")
+                await self.tap('claim_reward_box')
+                await self.tap('exit_encounter')
+                actions_so_far = 0
 
             next_quest = quest_list[num + 1]
             next_quest_coords = splitCoords(next_quest)
 
             cooldown_until_next_stop = calculateCD(calculate(*quest_coords, *next_quest_coords)) * 60  # in seconds
-            time_so_far = time.time() - time_start
-            total_time_to_wait = max(0, cooldown_until_next_stop - time_so_far) + 5  # extra 30 seconds
+            total_time_to_wait = max(0, cooldown_until_next_stop) + 5  # extra 5 seconds
 
-            logger.info('Cooldown until next PokeStop is {} seconds.'.format(total_time_to_wait))
-            await asyncio.sleep(cooldown_until_next_stop - time_so_far)
+            logger.info('Cooldown until next PokeStop is %s seconds.', format(total_time_to_wait))
+            await asyncio.sleep(cooldown_until_next_stop)
 
 
 if __name__ == '__main__':
