c7a40d453da0c2b9fe3aabcf57b9ee3ccd038e5a
Emiliano Sauvisky <esauvisky@gmail.com>
2019-05-04 17:16:01 -0300

    Fixing debug thing

--- questr.py
+++ questr.py
@@ -55,6 +55,9 @@ class Main:
         tools = pyocr.get_available_tools()
         self.tool = tools[0]
         self.p = PokemonGo()
+        if args.debug:
+            logger.setLevel(logging.DEBUG)
+            self.debug = True
 
     async def hue_affinity(self, im, hue1, hue2):
         '''Checks the affinity, in percentual terms,
@@ -253,10 +256,6 @@ class Main:
 
 
     async def start(self):
-        if args.debug:
-            logger.setLevel(logging.DEBUG)
-            self.debug = True
-
         await self.p.set_device(self.args.device_id)
 
         with open('quest_list.txt', 'r') as file:
